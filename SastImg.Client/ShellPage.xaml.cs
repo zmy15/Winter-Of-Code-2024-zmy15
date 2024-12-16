ffcc516da23f30b13359b7186b9ed193d8adc391
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-12-16 22:43:04 +0800

    feat(ui): Shell page

---
+++ SastImg.Client/ShellPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SastImg.Client;
+public sealed partial class ShellPage : Page
+{
+    public ShellPage ( )
+    {
+
+        this.InitializeComponent();
+    }
+}
