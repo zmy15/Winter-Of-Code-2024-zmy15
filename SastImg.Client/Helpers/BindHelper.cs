cf2ae07237969869d5b025f0882580bb774915e6
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2025-01-13 13:54:05 +0800

    feat: Nuget package

--- SastImg.Client/Helpers/BindHelper.cs
+++ SastImg.Client/Helpers/BindHelper.cs
@@ -1,9 +1,16 @@
-﻿namespace SastImg.Client.Helpers;
+﻿using Microsoft.UI.Xaml;
+
+namespace SastImg.Client.Helpers;
 
 public static partial class BindHelper
 {
     public static bool Not (bool value) // x:Bind 时对值取非使用
     {
         return !value;
+    }
+
+    public static Visibility BoolToVisibility (bool value, bool inversed) // x:Bind 时对值转换为 Visibility 使用
+    {
+        return inversed ? (value ? Visibility.Collapsed : Visibility.Visible) : (value ? Visibility.Visible : Visibility.Collapsed);
     }
 }
