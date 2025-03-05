03285db550d568ece813893c72f66cd11b5d6c1c
zmy15 <zmy15146892365@163.com>
2025-03-06 01:08:48 +0800

    feat:清理缓存

--- SastImg.Client/Views/HomeViewModel.cs
+++ SastImg.Client/Views/HomeViewModel.cs
@@ -6,14 +6,50 @@ using SastImg.Client.Services;
 using System.Collections.Generic;
 using System.Windows.Input;
 using System.IO;
-using System.Drawing;
+using Windows.Storage;
+using System.Diagnostics;
 
 namespace SastImg.Client.Views;
 
-public class HomeViewModel : ObservableObject
+public partial class HomeViewModel : ObservableObject
 {
-public HomeViewModel()
+    public HomeViewModel()
     {
 
+    }
+    [ObservableProperty]
+    private string _massage;
+    [ObservableProperty]
+    private string _color;
+
+    public ICommand ClearCache => new RelayCommand(async () => {
+        if(await DeleteAllFilesInLocalFolderAsync())
+        {
+            Massage = "清理成功";
+            Color = "Green";
+        }
+        else
+        {
+            Massage = "清理失败";
+            Color = "Red";
+        }
+    });
+    public static async Task<bool> DeleteAllFilesInLocalFolderAsync()
+    {
+        try
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            var files = await localFolder.GetFilesAsync();
+            foreach (var file in files)
+            {
+                await file.DeleteAsync();
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"删除文件时发生错误: {ex.Message}");
+            return false;
+        }
     }
 }
