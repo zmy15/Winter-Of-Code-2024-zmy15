b7f21651909ef62226cb0e8e3020e7091c68cc29
zmy15 <zmy15146892365@163.com>
2025-03-09 19:25:05 +0800

    update

--- SastImg.Client/Views/AlbumDetailView.xaml.cs
+++ SastImg.Client/Views/AlbumDetailView.xaml.cs
@@ -1,22 +1,7 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
-using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,7 +17,6 @@ namespace SastImg.Client.Views
             InitializeComponent();
             DataContext = ViewModel;
         }
-        //·µ»Ø°´Å¥
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (App.Shell.MainFrame.CanGoBack)
